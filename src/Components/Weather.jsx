3ae863f1952933dde0048fe0188112fd06576946
Jainik Darji <darjijainik@gmail.com>
2024-06-07 14:20:02 +0530

    addd

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -43,7 +43,7 @@ const Weather = () => {
       return;   
     }
     try {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${ 
         import.meta.env.VITE_APP_ID
       }`;
 
